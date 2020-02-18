aa0ac8a93c0fcef4a8f6de2116238cadc1bcbb24
Jocelyn Chang <jocelync1094@gmail.com>
2020-02-17 17:12:14 -0800

    header component created with search bar

--- employeedirectory/src/components/Header.js
+++ employeedirectory/src/components/Header.js
@@ -1,9 +1,23 @@
 import React from "react";
 
-const Header = () => {
-  return (
-
-  )
+const style = {
+    head: {
+        color: "white",
+        fontWeight: "bold",
+        fontFamily: "Times New Roman, Times, serif"
+    }
 }
 
+const Header = () => {
+return (
+    <nav className="navbar navbar-light bg-dark">
+      <a className="navbar-brand" href="/" style={style.head}>Employee Directory</a>
+      <form className="form-inline"> 
+      <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+      <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+      </form>
+    </nav>
+    )
+}
+          
 export default Header;
